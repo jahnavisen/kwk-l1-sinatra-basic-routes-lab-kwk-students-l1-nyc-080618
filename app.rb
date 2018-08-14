146f34419ea13ff201ace1df98ee585f6c8ee85b
jahnavisen <jahnavi.sen02@gmail.com>
2018-08-14 18:54:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,5 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   get '/' do 
     "My name is Jahnavi"
-    "My hometown is "
+    "My hometown is Jersey City"
+    "My favorite song is Lone Ranger"
+  end 
 end
